43b4d02b55891c8f85a85397f30e2d7619a2b661
t3hVega <ivan.laganchuk@gmail.com>
2023-02-18 10:00:49 +0300

    Ввел правки в ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         tasks1_2_3();
+        task4();
         System.out.println("ДЗ сделано");
     }
     public static int[] generateRandomArray() {
@@ -19,21 +20,26 @@ public class Main {
             overallSpendings += spendings[i];
         }
         System.out.println("Сумма трат за месяц составила " + overallSpendings + " рублей");
-        int minSpendings = 200001;
-        int maxSpendings = 99999;
+        int minSpendings = spendings[0];
+        int maxSpendings = spendings[0];
         for (int i = 0; i < spendings.length; i++) {
             if (spendings[i] > maxSpendings){
                 maxSpendings = spendings[i];
             }
-        }
-        System.out.println("Максимальная сумма трат за день составила " + maxSpendings + " рублей");
-        for (int i = 0; i < spendings.length; i++) {
             if (spendings[i] < minSpendings){
                 minSpendings = spendings[i];
             }
         }
+        System.out.println("Максимальная сумма трат за день составила " + maxSpendings + " рублей");
         System.out.println("Минимальная сумма трат за день составила " + minSpendings + " рублей");
-        double midSpendings = overallSpendings / 30;
+        double midSpendings = overallSpendings / spendings.length;
         System.out.println("Средняя сумма трат за день составила " + midSpendings + " рублей");
+    }
+    public static void task4() {
+        char [] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
+        System.out.println();
     }
 }
